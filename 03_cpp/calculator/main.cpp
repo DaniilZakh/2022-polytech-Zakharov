1d3b6fb46d4f559345e83ee34adf05d6a4bd1f43
DaniilZakh <112750269+DaniilZakh@users.noreply.github.com>
2022-12-09 14:20:27 +0300

    Update main.cpp

--- 03_cpp/calculator/main.cpp
+++ 03_cpp/calculator/main.cpp
@@ -1,20 +1,61 @@
-#include <iostream>
-#include "calculator.h"
-
+#include<iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-    double a, b, result;
-    char $;
-    cout << "Calculator\n Enter operations in a$b format\n";
-    while(true)
+    int FirstNumber;
+    int SecondNumber;
+    int sum;
+    int minus;
+    int prod;
+    int div;
+    int answer;
+
+    char Operation;
+
+    cout << "Введите 1-ое число" <<"\n";
+    cin >> FirstNumber;
+    cout << "Введите 2-ое число" << "\n";
+    cin >> SecondNumber;
+
+    cout << "Выберите одну из операций: +, -, *, /, ^" << "\n";
+    cin >> Operation;
+    
+    if (Operation == '+')
     {
-        cin >> a >> $ >> b;
-        result = calculate(a, $, b);
-        cout << a << $ << b << "=" << result << endl;
+        sum = FirstNumber + SecondNumber;
+        cout << "Сумма чисел = " << sum << "\n";
+    }
+    if (Operation == '-')
+    {
+        minus = FirstNumber - SecondNumber;
+        cout << "Разность чисел = " << minus << "\n";
+    }
+    if (Operation == '*')
+    {
+        prod = FirstNumber * SecondNumber;
+        cout << "Произведение чисел = " << prod << "\n";
+    }
+    if (Operation == '/')
+    {
+        div = FirstNumber / SecondNumber;
+        cout << "Частное чисел = " << div << "\n";
+    }
+    if (Operation == '^')
+    {
+        cout << "Возведение числа " << FirstNumber << " в степень " << SecondNumber << " = " << pow(FirstNumber, SecondNumber) << "\n";
+    }
+
+    cout << "Повторить? 1 - да, 2 - нет"  "\n";
+    cin  >> answer;
+
+    if (answer ==1)
+    {
+        main();
+    }
+    else{
+        return 0;
     }
     
-    return 0;
-
 }
