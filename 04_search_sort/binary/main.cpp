7d5dd885c2d616b43b4aa7158f326d531c30ab7e
DaniilZakh <112750269+DaniilZakh@users.noreply.github.com>
2022-12-16 14:11:50 +0300

    Update main.cpp

--- 04_search_sort/binary/main.cpp
+++ 04_search_sort/binary/main.cpp
@@ -6,6 +6,6 @@ int main()
     int arr[] = {5, 6, 7, 8, 9, 13, 25};
     const int arr_size = sizeof(arr) / sizeof(arr[0]);
     int x = 9;
-    std::cout << search(arr, arr_size, x) << std::endl;
+    std::cout << search(arr, arr_size, x) + 1 << std::endl;
     return 0;
 } 
